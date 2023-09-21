613dc3448e883cf34d7f9a27ebcf68f5781ec13a
LAURIER Alexis <19586755+AlexisLaurier@users.noreply.github.com>
2023-09-21 20:05:51 +0200

    Some correction of init

--- src/extension.js
+++ src/extension.js
@@ -1,34 +1,47 @@
-/**
- * Disable Gestures 2021
+/* extension.js
  *
- * A GNOME extension that disables built-in gestures. Useful for kiosks and touch screen apps.
- * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 /* exported init */
 
-export default class Extension {
-  enable () {
-    global.stage.get_actions().forEach(a => { a.enabled = false })
-    const disableUnmaximizeGesture = () => {
-      global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = false } });
-      global.stage.remove_action_by_name('osk');
+class Extension {
+    constructor() {
     }
-    global.display.connect('notify::focus-window', disableUnmaximizeGesture)
-    global.display.connect('in-fullscreen-changed', disableUnmaximizeGesture)
 
-  }
-
-  disable () {
-    global.stage.get_actions().forEach(a => { a.enabled = true })
-    const enableUnmaximizeGesture = () => {
-      global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = true } })
+    enable() {
+    	global.stage.get_actions().forEach(a => { a.enabled = false })
+    	const disableUnmaximizeGesture = () => {
+      		global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = false } });
+      		global.stage.remove_action_by_name('osk');
+    	}
+    	global.display.connect('notify::focus-window', disableUnmaximizeGesture)
+    	global.display.connect('in-fullscreen-changed', disableUnmaximizeGesture)
     }
-    global.display.connect('notify::focus-window', enableUnmaximizeGesture)
-    global.display.connect('in-fullscreen-changed', enableUnmaximizeGesture)
-  }
+
+    disable() {
+     	global.stage.get_actions().forEach(a => { a.enabled = true })
+    	const enableUnmaximizeGesture = () => {
+      		global.stage.get_actions().forEach(a => { if (a !== this) { a.enabled = true } })
+    	}
+    	global.display.connect('notify::focus-window', enableUnmaximizeGesture)
+    	global.display.connect('in-fullscreen-changed', enableUnmaximizeGesture)
+    }
 }
 
-function init () {
-  return new Extension()
+function init() {
+    return new Extension();
 }
